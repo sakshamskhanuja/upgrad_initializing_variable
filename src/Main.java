2964012dba1bd824fcebedd2826a0edb8498e1b6
Saksham Singh Khanuja <sskhanuja94@gmail.com>
2023-01-08 14:45:49 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Initializing an int variable.
+        int firstVariable = 100;
+        System.out.println(firstVariable);
+    }
+}
